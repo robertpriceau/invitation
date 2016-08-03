ce046445ceba06c1102e5f4ecc255984cf8412e6
Robert Price <robert.price@myob.com>
2016-08-04 09:56:08 +1000

    Added Travis CI

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var invitation = require('./index');
+
+describe('invitation', function(){
+	it('should work!', function(){
+		expect(true).to.be.true;
+	});
+});
